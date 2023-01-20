9ef3a94912a1f67b617cd7b74fba225485473b00
Lisa Dafny <lisadafny.tech@gmail.com>
2023-01-20 00:51:21 -0300

    feature: modal to create new task card

--- site.js
+++ site.js
@@ -1,8 +1,32 @@
 $(document).ready(function(){
     $('#toDrag').draggable();
-    $('#btnAddNewCard').on('click', dontWork)
+    $('#btnAddNewCard').on('click', openModalNewCard);
+    $('.close-modal-nc').on('click', closeModalNewCard);
+    $('#btnCreateTask').on('click', dontWork)
+    $('input, textarea').on('focus', function () {
+        labelTopPosition(this);
+    });
+    $('input, textarea').on('focusout', function () {
+        changeLabelPosition(this);
+    });
 })
 
 function dontWork (){
-    alert('I do not work, yet :(')
+    alert('Sorry. I do not work, yet :(')
+}
+
+function labelTopPosition({ id }){
+    $(`label[for='${id}'`).addClass('label-on-top');
+}
+function changeLabelPosition({ id }){
+    let content = $(`#${id}`).val();
+    if(!content){
+        $(`label[for='${id}'`).removeClass('label-on-top');
+    }
+}
+function openModalNewCard(){
+ $('#modalNewCard').removeClass('d-none')
+}
+function closeModalNewCard(){
+$('#modalNewCard').addClass('d-none');
 }
